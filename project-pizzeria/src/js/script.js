f10b3953a8f198a303c5a35603869210a18d2368
MarcinZajecki <mar.zaj327@gmail.com>
2020-12-08 17:21:23 +0100

    Add products

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -52,14 +52,48 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  // 
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
+      console.log('new product:', thisProduct);
+    }
+    renderInMenu() {
+      const thisProduct = this;
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      menuContainer.appendChild(thisProduct.element);
+    }
+  }
+
   const app = {
-    init: function(){
+    initData: function () {
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+
+    initMenu: function () {
+      const thisApp = this;
+      console.log('thisApp.data', thisApp.data);
+      for (const productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
