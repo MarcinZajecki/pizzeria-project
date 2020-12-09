a99186db353282bf42180f3551df76d6b61fc678
MarcinZajecki <mar.zaj327@gmail.com>
2020-12-09 10:36:31 +0100

    Add accordion in menu positions

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -18,6 +18,7 @@
     },
     menuProduct: {
       clickable: '.product__header',
+      clickableElement: '.product__header i',
       form: '.product__order',
       priceElem: '.product__total-price .price',
       imageWrapper: '.product__images',
@@ -59,6 +60,7 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       console.log('new product:', thisProduct);
     }
     renderInMenu() {
@@ -67,6 +69,25 @@
       const menuContainer = document.querySelector(select.containerOf.menu);
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
       menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion() {
+      const thisProduct = this;
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickableElement);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function (event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProdPos = document.querySelectorAll('article.active');
+        for (let singlActiveProdPos of activeProdPos) {
+          /* if there is active product and it's not thisProduct.element, remove class active from it */
+          if (singlActiveProdPos !== thisProduct.element) singlActiveProdPos.classList.remove('active');
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
+      });
     }
   }
 
